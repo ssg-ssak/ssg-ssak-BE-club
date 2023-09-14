249dff1229a4838ecab2eccf35275a01819ef1fa
kklee0930 <kklee0930@gmail.com>
2023-09-14 17:19:31 +0900

    fix: corsConfig crossorigin all 적용

--- src/main/java/ssgssak/ssgpointappclub/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointappclub/global/config/security/CorsConfig.java
@@ -1,12 +1,14 @@
 package ssgssak.ssgpointappclub.global.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@CrossOrigin("*") //todo: 삭제요망. 테스트용
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
