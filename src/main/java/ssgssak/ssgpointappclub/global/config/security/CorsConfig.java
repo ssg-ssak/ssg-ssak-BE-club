72feb6c3de41f24412dcb4a0c12affccb0990414
kklee0930 <kklee0930@gmail.com>
2023-09-14 17:39:57 +0900

    fix: corsconfig 수정

--- src/main/java/ssgssak/ssgpointappclub/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointappclub/global/config/security/CorsConfig.java
@@ -1,23 +1,21 @@
 package ssgssak.ssgpointappclub.global.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@CrossOrigin("*") //todo: 삭제요망. 테스트용
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
+        registry.addMapping("/**")
                 .allowCredentials(true).maxAge(3600)
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
     }
+
 }
