191530c232ff787e9c0ad7478be2bcf1389e8f6d
Hwan0518 <qip0518@naver.com>
2023-09-15 10:32:03 +0900

    fix: securityConfig에서 PreFlightRequest요청을 시큐리티 무시하도록 설정

--- src/main/java/ssgssak/ssgpointappclub/global/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/ssgssak/ssgpointappclub/global/config/jwt/JwtAuthenticationFilter.java
@@ -6,9 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import ssgssak.ssgpointappclub.global.common.exception.TokenExpiredException;
@@ -30,25 +30,25 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         log.info("authHeader is : {}", authHeader);
         // 헤더가 null이거나, "Bearer"로 시작하지 않는다면 토큰 인증을 진행하지 않음
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-//            filterChain.doFilter(request, response); //todo:임시
+            filterChain.doFilter(request, response); //todo:임시
             throw new TokenExpiredException();
         }
 
         // 토큰 인증 진행
         jwt = authHeader.substring(7); // "Bearer " 뒷부분이 모두 jwt 토큰임
-//        uuid = jwtTokenProvider.getUUID(jwt);    // 토큰에서 uuid를 추출
-//        log.info("jwt is : {}", jwt);
-//        log.info("uuid is : {}", uuid);
-        // 만약 loginId가 null이 아니고, SecurityContextHolder에 authentication이 없고, 토큰이 유효성 검사를 통과했다면 authentication 생성
-//        if (uuid != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            // 여기서 credentials는 필요하다면 추가적으로 인증을 설정할 요소이다. credentials를 사용하지 않더라도 null을 입력해주어야한다
-//            UsernamePasswordAuthenticationToken authenticationToken =
-//                    new UsernamePasswordAuthenticationToken(uuid, null, null);
-//            // 해당 ip에서 의심스러운 활동이 감지되면, 해당 ip의 모든 요청을 차단하도록 정책을 설정할 수 있음!
-//            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//            // SecurityContextHolder에 authentication을 집어넣음으로써, 추후에 로그인한 유저의 정보에 접근할 수 있음
-//            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//            filterChain.doFilter(request, response);
-//        }
+        uuid = jwtTokenProvider.getUUID(jwt);    // 토큰에서 uuid를 추출
+        log.info("jwt is : {}", jwt);
+        log.info("uuid is : {}", uuid);
+//         만약 loginId가 null이 아니고, SecurityContextHolder에 authentication이 없고, 토큰이 유효성 검사를 통과했다면 authentication 생성
+        if (uuid != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // 여기서 credentials는 필요하다면 추가적으로 인증을 설정할 요소이다. credentials를 사용하지 않더라도 null을 입력해주어야한다
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(uuid, null, null);
+            // 해당 ip에서 의심스러운 활동이 감지되면, 해당 ip의 모든 요청을 차단하도록 정책을 설정할 수 있음!
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // SecurityContextHolder에 authentication을 집어넣음으로써, 추후에 로그인한 유저의 정보에 접근할 수 있음
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            filterChain.doFilter(request, response);
+        }
     }
 }
