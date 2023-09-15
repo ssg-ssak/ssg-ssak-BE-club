524f40477ca96de62bfb442e99a4be59c48417b1
Hwan0518 <qip0518@naver.com>
2023-09-15 10:40:26 +0900

    fix: CorsConfig 수정

--- src/main/java/ssgssak/ssgpointappclub/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointappclub/global/config/security/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true).maxAge(3600)
-                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
